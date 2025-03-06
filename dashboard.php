5348375ed55c185d7c81643d9a5e89f48592facd
Jjansennn <jansenjansenkhang@gmail.com>
2025-03-06 10:40:07 +0700

    dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="shortcut icon" href="assets/img/Logobgwhite.png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Parking Management System - Comprehensive Dashboard</title>
